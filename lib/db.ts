096b4ce4381519d800e4beed7a54f8d60b8f1595
Jero11twMac <jero11tw@gmail.com>
2025-07-07 22:53:11 -0300

    alpha-3.9-4 (fix for supabase)

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,19 @@
+// // lib/db.ts local
+// import { Pool } from "pg"
+
+// export const db = new Pool({
+//   user: "eduflow",
+//   host: "localhost",
+//   database: "eduflow",
+//   password: "eduflow123",
+//   port: 5432,
+// })
 // lib/db.ts
 import { Pool } from "pg"
 
 export const db = new Pool({
-  user: "eduflow",
-  host: "localhost",
-  database: "eduflow",
-  password: "eduflow123",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Necesario para Supabase
+  },
 })
